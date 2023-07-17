7e66531aa2cfc754eede86070e46a19122a21113
Aleksey <a.babykiny@gmail.com>
2023-07-17 17:30:07 +0300

    daily_challenge

---
+++ Di-Bootcamp/Week_2/Day_2/Daily_Challenge.py
@@ -0,0 +1,23 @@
+# Challenge 1
+# Ask the user for a number and a length.
+# Create a program that prints a list of multiples of the number until the list length reaches length.
+
+# number1 = int(input("write Number\n"))
+# lenhht = int(input("write lengt\n"))
+# temp = 1
+# a = []
+# for i in range(lenhht):
+    
+#     a.append(number1*temp)
+#     temp += 1
+# print(a)    
+
+# Challenge 2
+# Write a program that asks a string to the user, and display a new string 
+# with any duplicate consecutive letters removed.
+
+# a = input("write string\n")
+# d = ""
+# for i in set(a):
+#     d += i
+# print(f"before {a} after {d}")    
--- Di-Bootcamp/Week_2/Day_2/Exercise _2.py
+++ Di-Bootcamp/Week_2/Day_2/Exercise _2.py
@@ -3,3 +3,4 @@
 # Given a tuple which value is integers, is it possible to add more integers to the tuple? impossible
 
 a = (1, 2 , 3 ,"c")
+
