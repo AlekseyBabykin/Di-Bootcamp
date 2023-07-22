30159425872c21f3e3ce4f1f460f4c64c923e91a
Aleksey <a.babykiny@gmail.com>
2023-07-22 12:37:19 +0300

    Fixed you can't bet on one square

--- Di-Bootcamp/Week_2/Day_5/1.py
+++ Di-Bootcamp/Week_2/Day_5/1.py
@@ -1,1 +1,10 @@
-print([1] == [1] )
+a = [[1,10],[2,3],[1,2]]
+c = [1,2]
+b = [1,2]
+a.append(c)
+print(a)
+# for i in a:
+#     if i == c:
+#         print("yes")
+print(c in a)
+print(c in b)
--- Di-Bootcamp/Week_2/Day_5/Mini-Project-Tic_Tac _Toe.py
+++ Di-Bootcamp/Week_2/Day_5/Mini-Project-Tic_Tac _Toe.py
@@ -5,6 +5,7 @@ display = [["*","*","*","1","*","*","*","*","2","*","*","*","*","3","*","*","*"]
            ["*"," "," ","-","-","-","|","-","-","-","|","-","-","-"," "," ","*"],
            ["3"," "," "," "," "," ","|"," "," "," ","|"," "," "," "," "," ","3"],
            ["*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*"]]
+all_values = []
 
 
 
@@ -22,20 +23,20 @@ def player_input():
     print("the first player's move")
     first_player_2 = int(input("write colum\n"))
     cordinate1 = convert_input(first_player_1,first_player_2)
-    display[cordinate1[0]][cordinate1[1]] = "x"
-    display_board() 
+    display[cordinate1[0]][cordinate1[1]] = "X"
+    display_board()
+    check_win() 
     print("the second player's move")
     second_player_1 = int(input("write row\n"))
     print("the second player's move") 
     second_player_2 = int(input("write colum\n"))
     cordinate2 = convert_input(second_player_1,second_player_2) 
-    display[cordinate2[0]][cordinate2[1]] = "y"     
+    display[cordinate2[0]][cordinate2[1]] = "O"     
     display_board()
         
 
 def convert_input(x,y):
-    all_values = []
-    values_now = []
+    values_now = [0,1]
     if x == 3:
         x = 5
     if y == 3:
@@ -48,32 +49,66 @@ def convert_input(x,y):
         x = 1
     if y == 1:
         y = 3 
-    values_now.append(x)
-    values_now.append(y)
-    all_values.append(values_now)
-    if all_values[0] > 0:
-        for i in all_values: 
-            if values_now in all_values:
-                x = int(input("place busy i need new row\n"))
-                y = int(input("place busy i need new colum\n"))
-                values_now[0] = x
-                values_now[1] = y         
-                all_values.append(values_now)
-                return [values_now[0],values_now[1]]
-            else:
-                all_values.append(values_now)              
-                return [values_now[0],values_now[1]]
-    # busy_all.append(values_now) 
-    # return [values_now[0],values_now[1]]                      
+    values_now[0] = x
+    values_now[1] = y
+    # if all_values == [0,0]:
+    #     all_values.append(values_now)
+    #     print("all_values == [0,0]")
+    #     return [values_now[0],values_now[1]]
+    if all_values == values_now:
+        print("you can't come here")
+        x = int(input("new row\n"))
+        y = int(input("new colum\n"))
+        if x == 3:
+            x = 5
+        if y == 3:
+            y = 13
+        if x == 2:
+            x = 3
+        if y == 2:
+            y = 8
+        if x == 1:
+            x = 1
+        if y == 1:
+            y = 3 
+        values_now[0] = x
+        values_now[1] = y        
+        all_values.append(values_now)
+        return [values_now[0],values_now[1]]
+    elif values_now in all_values:
+        print("you can't come here") 
+        x = int(input("new row\n"))
+        y = int(input(" new colum\n"))
+        if x == 3:
+            x = 5
+        if y == 3:
+            y = 13
+        if x == 2:
+            x = 3
+        if y == 2:
+            y = 8
+        if x == 1:
+            x = 1
+        if y == 1:
+            y = 3 
+        values_now[0] = x
+        values_now[1] = y        
+        all_values.append(values_now)
+        return [values_now[0],values_now[1]]
+    else:    
+        all_values.append(values_now)
+        print(all_values)              
+        return [values_now[0],values_now[1]]
+                  
                
 
 
 
 def check_win():
     
-    if display[1][3] == display[1][8] == display[1][13]== "x" or display[3][3] == display[3][8] == display[3][13]== "x" or  display[5][3] == display[5][8] == display[5][13] == "x" or display[1][3] == display[3][3] == display[5][3] == "x" or display[1][8] == display[3][8] == display[5][8] == "x" or display[1][13] == display[3][13] == display[5][13] == "x" or display[1][3] == display[3][8] == display[5][13]== "x" or display[5][3] == display[3][8] == display[1][13]== "x":
+    if display[1][3] == display[1][8] == display[1][13]== "X" or display[3][3] == display[3][8] == display[3][13]== "X" or  display[5][3] == display[5][8] == display[5][13] == "X" or display[1][3] == display[3][3] == display[5][3] == "X" or display[1][8] == display[3][8] == display[5][8] == "X" or display[1][13] == display[3][13] == display[5][13] == "X" or display[1][3] == display[3][8] == display[5][13]== "X" or display[5][3] == display[3][8] == display[1][13]== "X":
         return 1
-    if display[1][3] == display[1][8] == display[1][13]== "y" or display[3][3] == display[3][8] == display[3][13]== "y" or  display[5][3] == display[5][8] == display[5][13] == "y" or display[1][3] == display[3][3] == display[5][3] == "y" or display[1][8] == display[3][8] == display[5][8] == "y" or display[1][13] == display[3][13] == display[5][13] == "y" or display[1][3] == display[3][8] == display[5][13]== "y" or display[5][3] == display[3][8] == display[1][13]== "y":
+    if display[1][3] == display[1][8] == display[1][13]== "O" or display[3][3] == display[3][8] == display[3][13]== "O" or  display[5][3] == display[5][8] == display[5][13] == "O" or display[1][3] == display[3][3] == display[5][3] == "O" or display[1][8] == display[3][8] == display[5][8] == "O" or display[1][13] == display[3][13] == display[5][13] == "O" or display[1][3] == display[3][8] == display[5][13]== "O" or display[5][3] == display[3][8] == display[1][13]== "O":
         return 2
         
 
